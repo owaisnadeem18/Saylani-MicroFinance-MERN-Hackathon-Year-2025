2f19e75c1f0861dbb0aa752d8f90e78c71838031
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-11-28 23:53:25 +0500

    Delete routes directory

--- routes/Guarantor.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { guarantorsInfo } from "../controllers/Guarantor.controller.js"
-
-const router = express.Router()
-
-router.post("/" , guarantorsInfo)
-
-export default router
