c2efca1387ebc6edf1fac0304f9ac7e6d0eed6e6
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-11-29 08:33:09 +0500

    updated changes and packages

--- Backend/controllers/User.controller.js
+++ Backend/controllers/User.controller.js
@@ -1,63 +1,77 @@
-// -------------------------------------------------------------------------- 1 -------------------------------------------------------------------------- 
+// -------------------------------------------------------------------------- 1 --------------------------------------------------------------------------
 
-// This is the register user API 
+// This is the register user API
 // Q. Where it will be used in frontend ?
-// Ans. When pop up will open to enter the information of the user before proceeding qarz e hasana application 
+// Ans. When pop up will open to enter the information of the user before proceeding qarz e hasana application
 
-import User from "../models/User.model.js"
-export const registerUser =  async (req , res) => {
+import User from "../models/User.model.js";
+import { sendEmail } from "../utils/sendEmail.js";
+export const registerUser = async (req, res) => {
+  try {
+    const { Name, Email, CNIC } = req.body;
 
-    try {
-        const {Name , Email , CNIC} = req.body
-
-        if (!Name || !Email || !CNIC) {
-            return res.status(400).json({
-                success: false,
-                message: "Something is missing ! "
-            })
-        }
-
-        // check if user already exists or not with CNIC ...
-
-        const isUserExisted = await User.findOne({CNIC})
-
-        if (isUserExisted) {
-            return res.status(400).json({
-                message: "User with this CNIC already exists!" ,
-                success: false,
-            }
-            )
-        }
-
-        // generate a random password , which backend will send to the user on it's email after successfull registration: 
-        const password = Math.random().toString(36).substring(2,8)
-
-        // If User does not exist, then register a new user in DB 
-
-        const newUser = await User.create({
-            Name , 
-            Email ,
-            CNIC , 
-            Password:password
-        })
-
-        return res.status(201).json({
-            message: "User registered successfully!" ,
-            success: true,
-            userId: newUser._id,
-            Email: newUser.Email,
-            CNIC: newUser.CNIC
-        })
-
+    if (!Name || !Email || !CNIC) {
+      return res.status(400).json({
+        success: false,
+        message: "Something is missing ! ",
+      });
     }
 
-    catch (err) {
+    // check if user already exists or not with CNIC ...
 
-        return res.status(500).json({
-            message: "Internal Server Error" ,
-            success: false,
-            error: err.message
-        })
+    const isUserExisted = await User.findOne({ CNIC });
 
+    if (isUserExisted) {
+      return res.status(400).json({
+        message: "User with this CNIC already exists!",
+        success: false,
+      });
     }
-}
+
+    // generate a random password , which backend will send to the user on it's email after successfull registration:
+    const password = Math.random().toString(36).substring(2, 8);
+
+    // If User does not exist, then register a new user in DB
+
+    const newUser = await User.create({
+      Name,
+      Email,
+      CNIC,
+      Password: password,
+    });
+
+    // official email message:
+    const message = `Dear ${Name},
+
+Your account for Saylani Microfinance (Qarz-e-Hasana) has been created successfully.
+Your login password is: ${password}
+
+Please keep it safe and do not share it with anyone.
+
+Regards,
+Saylani Welfare Qarz-e-Hasana Team`;
+
+    // send email to user: (system generated password)
+
+    await sendEmail(
+      Email,
+      "Your Qarz-e-Hasana Account Password",
+        message
+    );
+
+    return res.status(201).json({
+      message: "User registered successfully!",
+      success: true,
+      userId: newUser._id,
+      Email: newUser.Email,
+      CNIC: newUser.CNIC,
+      Password: password,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+      error: err.message,
+    });
+  }
+};
---
+++ Backend/utils/sendEmail.js
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer"
+
+export const sendEmail = async (to , subject , text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Qarz-e-Hasana" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("📧 Email sent successfully to:", to);
+
+    }
+    catch (err) {
+        
+    }
+}
