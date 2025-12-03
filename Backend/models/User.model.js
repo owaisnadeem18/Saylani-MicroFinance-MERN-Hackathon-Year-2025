48ffc35cc6c3a046b53023b20e448c3b0de2f544
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-12-03 16:35:59 +0500

    updated changes in User API model

--- Backend/models/User.model.js
+++ Backend/models/User.model.js
@@ -6,8 +6,9 @@ const userSchema = mongoose.Schema({
     Email : {type: String , required: true , unique: true},
     Password: {type: String , required: true} ,
     role: {type: String , enum: ["admin" , "user" ] , default: "user" } ,
-    DateCreated : {type: Date , default: Date.now}
-})
+    DateCreated : {type: Date , default: Date.now} ,
+    mustChangePassword: {type: Boolean , default : true} // flag of must change the password 
+} , {timeStamps : true} )
 
 const User = mongoose.model("User" , userSchema)
 export default User
