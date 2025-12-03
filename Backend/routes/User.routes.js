b744132b446913984c21bad22e6be180ddde1ee6
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-12-03 17:56:39 +0500

    login user route

--- Backend/routes/User.routes.js
+++ Backend/routes/User.routes.js
@@ -1,12 +1,15 @@
 import express from "express"
 
 // Now , import Controller functions from User.controller.js file:
-import { registerUser } from "../controllers/User.controller.js"
+import { loginUser, registerUser } from "../controllers/User.controller.js"
 
 
 const router = express.Router()
 
 // route # 01 of User Registration API:
 router.post("/register" , registerUser)
+
+// route # 02 of User Login: 
+router.post("/login" , loginUser)
 
 export default router
