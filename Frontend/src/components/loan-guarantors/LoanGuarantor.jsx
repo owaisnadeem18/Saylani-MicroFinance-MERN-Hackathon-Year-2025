e9c91c338b40465f0486cbd5f4d195b508176ef0
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-01-22 14:26:36 +0500

    Update LoanGuarantor.jsx

--- Frontend/src/components/loan-guarantors/LoanGuarantor.jsx
+++ Frontend/src/components/loan-guarantors/LoanGuarantor.jsx
@@ -5,7 +5,7 @@ import GuarantorsForm from './GuarantorsForm'
 const LoanGuarantor = () => {
   return (
     <div className='bg-gray-50 min-h-[calc(100vh-88px)]' >
-        <div className='container mx-auto max-w-7xl p-12 flex flex-col gap-12'  >
+        <div className='container mx-auto max-w-7xl p-12 flex flex-col gap-12'>
             <LoanGuarantorsBanner/>
             <GuarantorsForm/>
         </div>
